5a76f930c0bbb31696fed4589b16ea84867c7ece
ascii <39356292+asciidude@users.noreply.github.com>
2022-07-20 22:54:07 -0500

    Update config.CHANGEME.lua

--- config.CHANGEME.lua
+++ config.CHANGEME.lua
@@ -16,9 +16,9 @@ Config.menuName = '911 Menu'
 Config.menuDescription = '~b~Report emergencies here'
 
 Config.useDiscord = true -- If true, it will NOT use in-game chat. I highly recommend turning this to true
-Config.webhookURL = 'https://discord.com/api/webhooks/999522066548137984/3pItT7BNdpgMeCJVF5KNcR40l-kzMwoYeYHSMGOStdcj8I3iQbj5YeWEPsQP-MTKBDed' -- The webhook of the channel to send to
+Config.webhookURL = '' -- The webhook of the channel to send to
 Config.webhookName = '911 Calls by asciidude'
-Config.webhookImage = 'https://icon-library.com/images/911-icon/911-icon-24.jpg' -- The icon of the webhook
+Config.webhookImage = '' -- The icon of the webhook, unsure if this works
 Config.webhookStartNotify = false -- Notify once the script starts
 
 -- Recommended to keep false - may slow down server upon loading menu!
