b15cdb665a5cd45a450ea6fdaaf1bfdbf77aad34
ascii <39356292+asciidude@users.noreply.github.com>
2022-07-26 19:30:23 -0500

    Fixing up optimization

--- server.lua
+++ server.lua
@@ -28,7 +28,7 @@ AddEventHandler('emergencymenu.submitMessage', function(message, source)
             TriggerClientEvent('chat:addMessage', id, {
                 color = { 255, 0, 0 },
                 multiline = true,
-                args = {'[911 | ' .. source .. ']', message}
+                args = {'^4911 | ' .. source .. '', message}
             })
 
             TriggerClientEvent('emergencymenu.addBlip', id)
@@ -37,7 +37,7 @@ AddEventHandler('emergencymenu.submitMessage', function(message, source)
         TriggerClientEvent('chat:addMessage', -1, {
             color = { 255, 0, 0 },
             multiline = true,
-            args = {'[911 | ' .. source ..']', message}
+            args = {'^4911 | ' .. source ..'', message}
         })
 
         TriggerClientEvent('emergencymenu.addBlip', -1)
@@ -71,6 +71,16 @@ AddEventHandler('emergencymenu.removePlayerBlip', function(blip, source)
         if t_blip.identifier == identifier then
             TriggerClientEvent('emergencymenu.removeBlip', -1, t_blip.blip)
             table.remove(blips, i)
+
+            break
+        end
+    end
+end)
+
+AddEventHandler('emergencymenu.removeCallMember', function(source)
+    for i, id in ipairs(callmembers) do
+        if id == source then
+            table.remove(callmembers, i)
 
             break
         end
